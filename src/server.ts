edee7eef8906f3d0cd1bea632554b7a19f2f221c
amel-harrath <amel.harrath.92@gmail.com>
2025-05-04 23:47:04 +0200

    chore: added validation

--- src/server.ts
+++ src/server.ts
@@ -53,7 +53,8 @@ const start = async (): Promise<void> => {
     });
 
     server.register(cors, {
-      origin: true,
+      origin: 'http://localhost:5173',
+      credentials: true,
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     });
     server.register(userRoutes, { prefix: '/api' });
